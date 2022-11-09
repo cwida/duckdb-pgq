3432030eeef4a883ea17cce1f5e33e21b5efd799
daniel <dljtw98@gmail.com>
2022-11-09 15:40:28 +0100

    Added csr class

--- src/include/duckdb/main/client_data.hpp
+++ src/include/duckdb/main/client_data.hpp
@@ -24,6 +24,8 @@ class PreparedStatementData;
 class SchemaCatalogEntry;
 struct RandomEngine;
 
+
+
 struct ClientData {
 	ClientData(ClientContext &context);
 	~ClientData();
@@ -51,6 +53,11 @@ struct ClientData {
 
 	//! The file search path
 	string file_search_path;
+
+
+	//! Used to build the CSR data structures required for path-finding queries
+	std::unordered_map<int32_t, unique_ptr<Csr>> csr_list;
+	std::mutex csr_lock;
 
 public:
 	DUCKDB_API static ClientData &Get(ClientContext &context);
