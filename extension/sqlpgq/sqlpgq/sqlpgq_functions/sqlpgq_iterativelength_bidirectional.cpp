80d573141f42c232fc8b821f9983fe7e7fdd2cf7
daniel <dljtw98@gmail.com>
2022-11-09 12:04:50 +0100

    Adding sqlpgq extension

---
+++ extension/sqlpgq/sqlpgq/sqlpgq_functions/sqlpgq_iterativelength_bidirectional.cpp
@@ -0,0 +1,178 @@
+#include "duckdb/parser/parsed_data/create_scalar_function_info.hpp"
+#include "duckdb/main/client_context.hpp"
+#include "duckdb/execution/expression_executor.hpp"
+#include "duckdb/planner/expression/bound_function_expression.hpp"
+#include "sqlpgq_functions.hpp"
+
+namespace duckdb {
+
+struct IterativeLengthBidirectionalBindData : public FunctionData {
+	ClientContext &context;
+	string file_name;
+
+	IterativeLengthBidirectionalBindData(ClientContext &context, string &file_name) : context(context), file_name(file_name) {
+	}
+
+	unique_ptr<FunctionData> Copy() override {
+		return make_unique<IterativeLengthBidirectionalBindData>(context, file_name);
+	}
+};
+
+static bool IterativeLengthBidirectional(int64_t v_size, int64_t *V, vector<int64_t> &E, vector<std::bitset<LANE_LIMIT>> &seen,
+                            vector<std::bitset<LANE_LIMIT>> &visit, vector<std::bitset<LANE_LIMIT>> &next) {
+	bool change = false;
+	for (auto v = 0; v < v_size; v++) {
+		next[v] = 0;
+	}
+	for (auto v = 0; v < v_size; v++) {
+		if (visit[v].any()) {
+			for (auto e = V[v]; e < V[v + 1]; e++) {
+				auto n = E[e];
+				next[n] = next[n] | visit[v];
+			}
+		}
+	}
+	for (auto v = 0; v < v_size; v++) {
+		next[v] = next[v] & ~seen[v];
+		seen[v] = seen[v] | next[v];
+		change |= next[v].any();
+	}
+	return change;
+}
+static std::bitset<LANE_LIMIT>
+InterSectFronteers(int64_t v_size, vector<std::bitset<LANE_LIMIT>> &src_seen, vector<std::bitset<LANE_LIMIT>> &dst_seen) {
+    std::bitset<LANE_LIMIT> result;
+    for (auto v = 0; v < v_size; v++) {
+        result |= src_seen[v] & dst_seen[v];
+    }
+    return result;
+}
+
+static void IterativeLengthBidirectionalFunction(DataChunk &args, ExpressionState &state, Vector &result) {
+	auto &func_expr = (BoundFunctionExpression &)state.expr;
+	auto &info = (IterativeLengthBidirectionalBindData &)*func_expr.bind_info;
+
+	// get csr info (TODO: do not store in context -- make global map in module that is indexed by id+&context)
+	int32_t id = args.data[0].GetValue(0).GetValue<int32_t>();
+	D_ASSERT(info.context.csr_list[id]);
+	int64_t v_size = args.data[1].GetValue(0).GetValue<int64_t>();
+	int64_t *v = (int64_t *)info.context.csr_list[id]->v;
+	vector<int64_t> &e = info.context.csr_list[id]->e;
+
+	// get src and dst vectors for searches
+	auto &src = args.data[2];
+	auto &dst = args.data[3];
+	VectorData vdata_src;
+	VectorData vdata_dst;
+	src.Orrify(args.size(), vdata_src);
+	dst.Orrify(args.size(), vdata_dst);
+	auto src_data = (int64_t *)vdata_src.data;
+	auto dst_data = (int64_t *)vdata_dst.data;
+
+	// create result vector
+	result.SetVectorType(VectorType::FLAT_VECTOR);
+    ValidityMask &result_validity = FlatVector::Validity(result);
+    auto result_data = FlatVector::GetData<int64_t>(result);
+
+	// create temp SIMD arrays
+	vector<std::bitset<LANE_LIMIT>> src_seen(v_size);
+	vector<std::bitset<LANE_LIMIT>> src_visit1(v_size);
+	vector<std::bitset<LANE_LIMIT>> src_visit2(v_size);
+	vector<std::bitset<LANE_LIMIT>> dst_seen(v_size);
+    vector<std::bitset<LANE_LIMIT>> dst_visit1(v_size);
+    vector<std::bitset<LANE_LIMIT>> dst_visit2(v_size);
+
+	// maps lane to search number
+	short lane_to_num[LANE_LIMIT];
+	for (int64_t lane = 0; lane < LANE_LIMIT; lane++) {
+		lane_to_num[lane] = -1; // inactive
+	}
+
+	idx_t started_searches = 0;
+	while (started_searches < args.size()) {
+
+		// empty visit vectors
+		for (auto i = 0; i < v_size; i++) {
+			src_seen[i] = 0;
+            dst_seen[i] = 0;
+			src_visit1[i] = 0;
+            dst_visit1[i] = 0;
+		}
+
+		// add search jobs to free lanes
+		uint64_t active = 0;
+		for (int64_t lane = 0; lane < LANE_LIMIT; lane++) {
+            lane_to_num[lane] = -1;
+			while (started_searches < args.size()) {
+				int64_t search_num = started_searches++;
+				int64_t src_pos = vdata_src.sel->get_index(search_num);
+                int64_t dst_pos = vdata_dst.sel->get_index(search_num);
+                if (!vdata_src.validity.RowIsValid(src_pos)) {
+                    result_validity.SetInvalid(search_num);
+                    result_data[search_num] = (uint64_t) - 1; // no path
+                } else if (src_data[src_pos] == dst_data[dst_pos]) {
+                    result_data[search_num] = (uint64_t) 0; // path of length 0 does not require a search
+				} else {
+                    src_visit1[src_data[src_pos]][lane] = 1;
+                    dst_visit1[dst_data[dst_pos]][lane] = 1;
+                    src_seen[src_data[src_pos]][lane] = 1;
+                    dst_seen[dst_data[dst_pos]][lane] = 1;
+                    lane_to_num[lane] = search_num; // active lane
+					active++;
+					break;
+				}
+			}
+		}
+
+		// make passes while a lane is still active
+		for (int64_t iter = 0; active; iter++) {
+            if (!IterativeLengthBidirectional(v_size, v, e,
+                                              (iter&1)?dst_seen:src_seen,
+                                              (iter&2)?(iter&1)?dst_visit2:src_visit2:(iter&1)?dst_visit1:src_visit1,
+                                              (iter&2)?(iter&1)?dst_visit1:src_visit1:(iter&1)?dst_visit2:src_visit2)) {
+				break;
+			}
+            std::bitset<LANE_LIMIT> done = InterSectFronteers(v_size, src_seen, dst_seen);
+            // detect lanes that finished
+			for (int64_t lane = 0; lane < LANE_LIMIT; lane++) {
+                if (done[lane]) {
+                    int64_t search_num = lane_to_num[lane];
+                    if (search_num >= 0) {
+                        result_data[search_num] = iter+1; /* found at iter => iter = path length */
+                        lane_to_num[lane] = -1;         // mark inactive
+                        active--;
+                    }
+				}
+			}
+		}
+		// no changes anymore: any still active searches have no path
+		for (int64_t lane = 0; lane < LANE_LIMIT; lane++) {
+			int64_t search_num = lane_to_num[lane];
+			if (search_num >= 0) {
+                result_validity.SetInvalid(search_num);
+				result_data[search_num] = (int64_t)-1; /* no path */
+				lane_to_num[lane] = -1;                // mark inactive
+			}
+		}
+	}
+}
+
+static unique_ptr<FunctionData> IterativeLengthBidirectionalBind(ClientContext &context, ScalarFunction &bound_function,
+                                                    vector<unique_ptr<Expression>> &arguments) {
+	string file_name;
+	if (arguments.size() == 5) {
+		file_name = ExpressionExecutor::EvaluateScalar(*arguments[4]).GetValue<string>();
+	} else {
+		file_name = "timings-test.txt";
+	}
+	return make_unique<IterativeLengthBidirectionalBindData>(context, file_name);
+}
+
+CreateScalarFunctionInfo SQLPGQFunctions::GetIterativeLengthBidirectionalFunction() {
+	auto fun = ScalarFunction("iterativelengthbidirectional",
+	                          {LogicalType::INTEGER, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT},
+	                          LogicalType::BIGINT, IterativeLengthBidirectionalFunction, false, IterativeLengthBidirectionalBind);
+	return CreateScalarFunctionInfo(fun);
+}
+
+} // namespace duckdb
