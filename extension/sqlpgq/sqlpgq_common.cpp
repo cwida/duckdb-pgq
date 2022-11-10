ccd23a07a37bbda068053e81186bebc5a7b3b900
daniel <dljtw98@gmail.com>
2022-11-10 10:03:57 +0100

    Adding iterative length to sqlpgq_common

--- extension/sqlpgq/sqlpgq_common.cpp
+++ extension/sqlpgq/sqlpgq_common.cpp
@@ -47,4 +47,25 @@ unique_ptr<FunctionData> CSRFunctionData::CSREdgeBind(ClientContext &context, Sc
 	}
 }
 
+
+unique_ptr<FunctionData> IterativeLengthFunctionData::Copy() const {
+	return make_unique<IterativeLengthFunctionData>(context, file_name);
+}
+
+bool IterativeLengthFunctionData::Equals(const FunctionData &other_p) const {
+	auto &other = (const IterativeLengthFunctionData &)other_p;
+	return file_name == other.file_name;
+}
+
+static unique_ptr<FunctionData> IterativeLengthFunctionData::IterativeLengthBind(ClientContext &context, ScalarFunction &bound_function,
+                                                    vector<unique_ptr<Expression>> &arguments) {
+	string file_name;
+	if (arguments.size() == 5) {
+		file_name = ExpressionExecutor::EvaluateScalar(*arguments[4]).GetValue<string>();
+	} else {
+		file_name = "timings-test.txt";
+	}
+	return make_unique<IterativeLengthFunctionData>(context, file_name);
+}
+
 } // namespace duckdb
--- extension/sqlpgq/sqlpgq_functions/sqlpgq_iterativelength.cpp
+++ extension/sqlpgq/sqlpgq_functions/sqlpgq_iterativelength.cpp
@@ -1,22 +1,12 @@
-#include "duckdb/parser/parsed_data/create_scalar_function_info.hpp"
-#include "duckdb/main/client_context.hpp"
 #include "duckdb/execution/expression_executor.hpp"
+#include "duckdb/main/client_context.hpp"
+#include "duckdb/main/client_data.hpp"
+#include "duckdb/parser/parsed_data/create_scalar_function_info.hpp"
 #include "duckdb/planner/expression/bound_function_expression.hpp"
+#include "sqlpgq_common.hpp"
 #include "sqlpgq_functions.hpp"
 
 namespace duckdb {
-
-struct IterativeLengthBindData : public FunctionData {
-	ClientContext &context;
-	string file_name;
-
-	IterativeLengthBindData(ClientContext &context, string &file_name) : context(context), file_name(file_name) {
-	}
-
-	unique_ptr<FunctionData> Copy() override {
-		return make_unique<IterativeLengthBindData>(context, file_name);
-	}
-};
 
 static bool IterativeLength(int64_t v_size, int64_t *v, vector<int64_t> &e, vector<std::bitset<LANE_LIMIT>> &seen,
                             vector<std::bitset<LANE_LIMIT>> &visit, vector<std::bitset<LANE_LIMIT>> &next) {
@@ -42,22 +32,22 @@ static bool IterativeLength(int64_t v_size, int64_t *v, vector<int64_t> &e, vect
 
 static void IterativeLengthFunction(DataChunk &args, ExpressionState &state, Vector &result) {
 	auto &func_expr = (BoundFunctionExpression &)state.expr;
-	auto &info = (IterativeLengthBindData &)*func_expr.bind_info;
+	auto &info = (IterativeLengthFunctionData &)*func_expr.bind_info;
 
 	// get csr info (TODO: do not store in context -- make global map in module that is indexed by id+&context)
 	int32_t id = args.data[0].GetValue(0).GetValue<int32_t>();
-	D_ASSERT(info.context.csr_list[id]);
+	D_ASSERT(info.context.client_data->csr_list[id]);
 	int64_t v_size = args.data[1].GetValue(0).GetValue<int64_t>();
-	int64_t *v = (int64_t *)info.context.csr_list[id]->v;
-	vector<int64_t> &e = info.context.csr_list[id]->e;
+	int64_t *v = (int64_t *)info.context.client_data->csr_list[id]->v;
+	vector<int64_t> &e = info.context.client_data->csr_list[id]->e;
 
 	// get src and dst vectors for searches
 	auto &src = args.data[2];
 	auto &dst = args.data[3];
-	VectorData vdata_src;
-	VectorData vdata_dst;
-	src.Orrify(args.size(), vdata_src);
-	dst.Orrify(args.size(), vdata_dst);
+	UnifiedVectorFormat vdata_src;
+	UnifiedVectorFormat vdata_dst;
+	src.ToUnifiedFormat(args.size(), vdata_src);
+	dst.ToUnifiedFormat(args.size(), vdata_dst);
 	auto src_data = (int64_t *)vdata_src.data;
 	auto dst_data = (int64_t *)vdata_dst.data;
 
@@ -92,12 +82,12 @@ static void IterativeLengthFunction(DataChunk &args, ExpressionState &state, Vec
 			while (started_searches < args.size()) {
 				int64_t search_num = started_searches++;
 				int64_t src_pos = vdata_src.sel->get_index(search_num);
-                int64_t dst_pos = vdata_dst.sel->get_index(search_num);
-                if (!vdata_src.validity.RowIsValid(src_pos)) {
+				int64_t dst_pos = vdata_dst.sel->get_index(search_num);
+				if (!vdata_src.validity.RowIsValid(src_pos)) {
 					result_data[search_num] = (uint64_t)-1; /* no path */
 				} else if (src_data[src_pos] == dst_data[dst_pos]) {
-                    result_data[search_num] = (uint64_t) 0;// path of length 0 does not require a search
-                } else {
+					result_data[search_num] = (uint64_t)0; // path of length 0 does not require a search
+				} else {
 					visit1[src_data[src_pos]][lane] = 1;
 					lane_to_num[lane] = search_num; // active lane
 					active++;
@@ -108,9 +98,9 @@ static void IterativeLengthFunction(DataChunk &args, ExpressionState &state, Vec
 
 		// make passes while a lane is still active
 		for (int64_t iter = 1; active; iter++) {
-//            std::cout << "Single direction iteration: " << iter << std::endl;
+			//            std::cout << "Single direction iteration: " << iter << std::endl;
 
-            if (!IterativeLength(v_size, v, e, seen, (iter&1)?visit1:visit2, (iter&1)?visit2:visit1)) {
+			if (!IterativeLength(v_size, v, e, seen, (iter & 1) ? visit1 : visit2, (iter & 1) ? visit2 : visit1)) {
 				break;
 			}
 			// detect lanes that finished
@@ -137,21 +127,10 @@ static void IterativeLengthFunction(DataChunk &args, ExpressionState &state, Vec
 	}
 }
 
-static unique_ptr<FunctionData> IterativeLengthBind(ClientContext &context, ScalarFunction &bound_function,
-                                                    vector<unique_ptr<Expression>> &arguments) {
-	string file_name;
-	if (arguments.size() == 5) {
-		file_name = ExpressionExecutor::EvaluateScalar(*arguments[4]).GetValue<string>();
-	} else {
-		file_name = "timings-test.txt";
-	}
-	return make_unique<IterativeLengthBindData>(context, file_name);
-}
-
 CreateScalarFunctionInfo SQLPGQFunctions::GetIterativeLengthFunction() {
-	auto fun = ScalarFunction("iterativelength",
-	                          {LogicalType::INTEGER, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT},
-	                          LogicalType::BIGINT, IterativeLengthFunction, false, IterativeLengthBind);
+	auto fun = ScalarFunction(
+	    "iterativelength", {LogicalType::INTEGER, LogicalType::BIGINT, LogicalType::BIGINT, LogicalType::BIGINT},
+	    LogicalType::BIGINT, IterativeLengthFunction, IterativeLengthFunctionData::IterativeLengthBind);
 	return CreateScalarFunctionInfo(fun);
 }
 
