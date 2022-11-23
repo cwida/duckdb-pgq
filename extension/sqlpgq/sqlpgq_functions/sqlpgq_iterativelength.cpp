432fc203602e301ec0b84fae8dc3472ddd568a35
dtenwolde <dljtw98@gmail.com>
2022-11-23 15:38:23 +0100

    Added debug prints and test cases

--- extension/sqlpgq/sqlpgq_functions/sqlpgq_iterativelength.cpp
+++ extension/sqlpgq/sqlpgq_functions/sqlpgq_iterativelength.cpp
@@ -4,6 +4,8 @@
 #include "sqlpgq_common.hpp"
 #include "sqlpgq_functions.hpp"
 
+#include <iostream>
+
 namespace duckdb {
 
 static bool IterativeLength(int64_t v_size, int64_t *v, vector<int64_t> &e, vector<std::bitset<LANE_LIMIT>> &seen,
@@ -12,6 +14,8 @@ static bool IterativeLength(int64_t v_size, int64_t *v, vector<int64_t> &e, vect
 	for (auto i = 0; i < v_size; i++) {
 		next[i] = 0;
 	}
+	std::cout << std::endl;
+
 	for (auto i = 0; i < v_size; i++) {
 		if (visit[i].any()) {
 			for (auto offset = v[i]; offset < v[i + 1]; offset++) {
@@ -20,11 +24,32 @@ static bool IterativeLength(int64_t v_size, int64_t *v, vector<int64_t> &e, vect
 			}
 		}
 	}
+
+	std::cout << "---------" << std::endl;
+
 	for (auto i = 0; i < v_size; i++) {
 		next[i] = next[i] & ~seen[i];
 		seen[i] = seen[i] | next[i];
 		change |= next[i].any();
 	}
+
+	for (auto i = 0; i < v_size; i++) {
+		std::cout << "Vertex: " << i << " seen: " << seen[i].count() << " next: " << next[i].count() << std::endl;
+	}
+
+	std::vector<int> _counter(LANE_LIMIT, 0);
+	for (auto i = 0; i < v_size; i++) {
+		for (auto j = 0; j < LANE_LIMIT; j++) {
+			if (next[i][j]) {
+				_counter[j]++;
+			}
+		}
+	}
+
+	for (auto i = 0; i < _counter.size(); i++) {
+		std::cout << "Lane " << i << " has discovered " << _counter[i] << " new node(s) this iteration" << std::endl;
+	}
+
 	return change;
 }
 
@@ -98,10 +123,14 @@ static void IterativeLengthFunction(DataChunk &args, ExpressionState &state, Vec
 			}
 		}
 
+		std::cout << std::endl << "Starting new batch of " << active
+		          << " searches. Filling up " << ((float)active / (float)LANE_LIMIT)*100 << "% of lanes." << std::endl;
+
+		auto _iterations = 1;
+		auto _finished_searches_total = 0;
 		// make passes while a lane is still active
 		for (int64_t iter = 1; active; iter++) {
-			//            std::cout << "Single direction iteration: " << iter << std::endl;
-
+			auto _finished_searches_iter = 0;
 			if (!IterativeLength(v_size, v, e, seen, (iter & 1) ? visit1 : visit2, (iter & 1) ? visit2 : visit1)) {
 				break;
 			}
@@ -111,17 +140,24 @@ static void IterativeLengthFunction(DataChunk &args, ExpressionState &state, Vec
 				if (search_num >= 0) { // active lane
 					int64_t dst_pos = vdata_dst.sel->get_index(search_num);
 					if (seen[dst_data[dst_pos]][lane]) {
+						std::cout << "Lane: " << lane << " finished after " << iter << " iteration(s)." << std::endl;
 						result_data[search_num] = iter; /* found at iter => iter = path length */
 						lane_to_num[lane] = -1;         // mark inactive
 						active--;
+						_finished_searches_total++;
+						_finished_searches_iter++;
 					}
 				}
 			}
+			_iterations++;
+			std::cout << "Finished " << _finished_searches_iter << " searches this iteration." << std::endl;
+			std::cout << "Finished " << _finished_searches_total << " searches so far." << std::endl;
 		}
 		// no changes anymore: any still active searches have no path
 		for (int64_t lane = 0; lane < LANE_LIMIT; lane++) {
 			int64_t search_num = lane_to_num[lane];
-			if (search_num >= 0) {                     // active lane
+			if (search_num >= 0) { // active lane
+				std::cout << "Lane " << lane << " did not find a path after " << _iterations << " iteration(s)." << std::endl;
 				result_validity.SetInvalid(search_num);
 				result_data[search_num] = (int64_t)-1; /* no path */
 				lane_to_num[lane] = -1;                // mark inactive
