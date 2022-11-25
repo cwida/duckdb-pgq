380a844b5557aeb041dc11869c097911410df909
dtenwolde <d.l.j.ten.wolde@student.vu.nl>
2022-11-25 12:25:12 +0100

    Added JEMALLOC to build

--- create_python_package.sh
+++ create_python_package.sh
@@ -14,4 +14,4 @@ virtualenv .venv
 source .venv/bin/activate
 python3 -m pip install pandas numpy jproperties
 
-EXTENSION_STATIC_BUILD=1 BUILD_SQLPGQ=1 BUILD_PYTHON=1 make GEN=ninja
+EXTENSION_STATIC_BUILD=1 BUILD_JEMALLOC=1 BUILD_SQLPGQ=1 BUILD_PYTHON=1 make GEN=ninja
