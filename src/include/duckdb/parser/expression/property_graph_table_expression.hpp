ec309f5e854793ad44a1ce60c1a4395f01818655
dtenwolde <dljtw98@gmail.com>
2023-02-11 11:18:40 +0100

    Removed Expression from PropertyGraphTable

--- src/include/duckdb/parser/expression/property_graph_table_expression.hpp
+++ src/include/duckdb/parser/expression/property_graph_table_expression.hpp
@@ -6,46 +6,28 @@
 //
 //===----------------------------------------------------------------------===//
 
-#pragma once
+//#pragma once
 
-#include "duckdb/parser/parsed_expression.hpp"
-#include "duckdb/common/vector.hpp"
+//#include "duckdb/parser/parsed_expression.hpp"
+//#include "duckdb/common/vector.hpp"
 
 namespace duckdb {
 
-//! Represents a reference to a column from either the FROM clause or from an
-//! alias
-class PropertyGraphTableExpression : public ParsedExpression {
+//! Represents a reference to a graph table from the CREATE PROPERTY GRAPH
+class PropertyGraphTable {
 public:
 	//! Specify both the column and table name
-	PropertyGraphTableExpression(string column_name, string label, string table_name);
+	PropertyGraphTable(string column_name, string label, string table_name);
 	//! Only specify the column name, the table name will be derived later
-	explicit PropertyGraphTableExpression(string column_name, string label);
+	explicit PropertyGraphTable(string column_name, string label);
 	//! Specify a set of names
-	explicit PropertyGraphTableExpression(vector<string> column_names, vector<string> labels);
+	explicit PropertyGraphTable(vector<string> column_names, vector<string> labels);
 
 	//! The stack of names in order of which they appear (column_names[0].column_names[1].column_names[2]....)
 	vector<string> column_names;
 
 	vector<string> labels;
 
-public:
-	bool IsQualified() const;
-	const string &GetColumnName() const;
-	const string &GetTableName() const;
-	bool IsScalar() const override {
-		return false;
-	}
-
-	string GetName() const override;
-	string ToString() const override;
-
-	static bool Equal(const PropertyGraphTableExpression *a, const PropertyGraphTableExpression *b);
-	hash_t Hash() const override;
-
-	unique_ptr<ParsedExpression> Copy() const override;
-
-	void Serialize(FieldWriter &writer) const override;
-//	static unique_ptr<ParsedExpression> Deserialize(ExpressionType type, FieldReader &source);
+	bool is_vertex_table;
 };
 } // namespace duckdb
--- src/parser/expression/property_graph_table_expression.cpp
+++ src/parser/expression/property_graph_table_expression.cpp
@@ -7,18 +7,17 @@
 
 namespace duckdb {
 
-PropertyGraphTableExpression::PropertyGraphTableExpression(string column_name, string label, string table_name)
-    : PropertyGraphTableExpression(table_name.empty() ? vector<string> {std::move(column_name)}, vector<string> {std::move(label)}
+PropertyGraphTable::PropertyGraphTable(string column_name, string label, string table_name)
+    : PropertyGraphTable(table_name.empty() ? vector<string> {std::move(column_name)}, vector<string> {std::move(label)}
                                              : vector<string> {std::move(table_name), std::move(column_name)}, vector<string> {std::move(label)}) {
 }
 
-PropertyGraphTableExpression::PropertyGraphTableExpression(string column_name, string label)
-    : PropertyGraphTableExpression(vector<string> {std::move(column_name)}, vector<string> {std::move(label)}) {
+PropertyGraphTable::PropertyGraphTable(string column_name, string label)
+    : PropertyGraphTable(vector<string> {std::move(column_name)}, vector<string> {std::move(label)}) {
 }
 
-PropertyGraphTableExpression::PropertyGraphTableExpression(vector<string> column_names_p, vector<string> labels_p)
-    : ParsedExpression(ExpressionType::COLUMN_REF, ExpressionClass::COLUMN_REF),
-      column_names(std::move(column_names_p)), labels(std::move(labels_p)) {
+PropertyGraphTable::PropertyGraphTable(vector<string> column_names_p, vector<string> labels_p)
+    : column_names(std::move(column_names_p)), labels(std::move(labels_p)) {
 #ifdef DEBUG
 	for (auto &col_name : column_names) {
 		D_ASSERT(!col_name.empty());
@@ -28,75 +27,6 @@ PropertyGraphTableExpression::PropertyGraphTableExpression(vector<string> column
 		D_ASSERT(!label.empty());
 	}
 #endif
-}
-
-bool PropertyGraphTableExpression::IsQualified() const {
-
-	return column_names.size() > 1;
-}
-
-const string &PropertyGraphTableExpression::GetColumnName() const {
-	D_ASSERT(column_names.size() <= 4);
-	return column_names.back();
-}
-
-const string &PropertyGraphTableExpression::GetTableName() const {
-	D_ASSERT(column_names.size() >= 2 && column_names.size() <= 4);
-	if (column_names.size() == 4) {
-		return column_names[2];
-	}
-	if (column_names.size() == 3) {
-		return column_names[1];
-	}
-	return column_names[0];
-}
-
-string PropertyGraphTableExpression::GetName() const {
-	return !alias.empty() ? alias : column_names.back();
-}
-
-string PropertyGraphTableExpression::ToString() const {
-	string result;
-	for (idx_t i = 0; i < column_names.size(); i++) {
-		if (i > 0) {
-			result += ".";
-		}
-		result += KeywordHelper::WriteOptionallyQuoted(column_names[i]);
-	}
-	return result;
-}
-
-bool PropertyGraphTableExpression::Equal(const PropertyGraphTableExpression *a, const PropertyGraphTableExpression *b) {
-	if (a->column_names.size() != b->column_names.size()) {
-		return false;
-	}
-	for (idx_t i = 0; i < a->column_names.size(); i++) {
-		auto lcase_a = StringUtil::Lower(a->column_names[i]);
-		auto lcase_b = StringUtil::Lower(b->column_names[i]);
-		if (lcase_a != lcase_b) {
-			return false;
-		}
-	}
-	return true;
-}
-
-hash_t PropertyGraphTableExpression::Hash() const {
-	hash_t result = ParsedExpression::Hash();
-	for (auto &column_name : column_names) {
-		auto lcase = StringUtil::Lower(column_name);
-		result = CombineHash(result, duckdb::Hash<const char *>(lcase.c_str()));
-	}
-	return result;
-}
-
-unique_ptr<ParsedExpression> PropertyGraphTableExpression::Copy() const {
-	auto copy = make_unique<PropertyGraphTableExpression>(column_names, labels);
-	copy->CopyProperties(*this);
-	return std::move(copy);
-}
-
-void PropertyGraphTableExpression::Serialize(FieldWriter &writer) const {
-	writer.WriteList<string>(column_names);
 }
 
 } // namespace duckdb
