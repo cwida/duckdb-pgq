a386a420cb31cef938a6cb22165066692248baba
dtenwolde <dljtw98@gmail.com>
2023-02-14 10:41:59 +0100

    Reworked constructor for PropertyGraphTable

--- src/parser/expression/property_graph_table_expression.cpp
+++ src/parser/expression/property_graph_table_expression.cpp
@@ -7,17 +7,8 @@
 
 namespace duckdb {
 
-PropertyGraphTable::PropertyGraphTable(string column_name, string label, string table_name)
-    : PropertyGraphTable(table_name.empty() ? vector<string> {std::move(column_name)}, vector<string> {std::move(label)}
-                                             : vector<string> {std::move(table_name), std::move(column_name)}, vector<string> {std::move(label)}) {
-}
-
-PropertyGraphTable::PropertyGraphTable(string column_name, string label)
-    : PropertyGraphTable(vector<string> {std::move(column_name)}, vector<string> {std::move(label)}) {
-}
-
-PropertyGraphTable::PropertyGraphTable(vector<string> column_names_p, vector<string> labels_p)
-    : column_names(std::move(column_names_p)), labels(std::move(labels_p)) {
+PropertyGraphTable::PropertyGraphTable(string table_name_p, vector<string> column_names_p, vector<string> labels_p)
+    : table_name(std::move(table_name_p)), column_names(std::move(column_names_p)), labels(std::move(labels_p)) {
 #ifdef DEBUG
 	for (auto &col_name : column_names) {
 		D_ASSERT(!col_name.empty());
