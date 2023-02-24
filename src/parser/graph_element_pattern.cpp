380b24bcb099041df2a3c800de55357fd17ec1f8
dtenwolde <dljtw98@gmail.com>
2023-02-24 09:54:37 +0100

    Serializer and deserializer for graphelementpattern

--- src/parser/graph_element_pattern.cpp
+++ src/parser/graph_element_pattern.cpp
@@ -1,5 +1,4 @@
 #include "duckdb/parser/graph_element_pattern.hpp"
-
 #include "duckdb/common/field_writer.hpp"
 
 
@@ -10,10 +9,17 @@ GraphElementPattern::GraphElementPattern() {
 }
 
 void GraphElementPattern::Serialize(Serializer &serializer) const {
+	FieldWriter writer(serializer);
+	writer.WriteSerializable<ParsedExpression>(*where_clause);
 }
+
 unique_ptr<GraphElementPattern> GraphElementPattern::Deserialize(Deserializer &deserializer) {
+	auto result = make_unique<GraphElementPattern>();
+	FieldReader reader(deserializer);
+	result->where_clause = reader.ReadRequiredSerializable<ParsedExpression>();
 	return unique_ptr<GraphElementPattern>();
 }
+
 bool GraphElementPattern::Equals(const GraphElementPattern *other_p) const {
 	if (!where_clause->Equals(other_p->where_clause.get())) {
 		return false;
