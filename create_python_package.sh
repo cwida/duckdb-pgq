4710215d3632dc09b3399ecbb1c4bad29b6e001b
dtenwolde <dljtw98@gmail.com>
2023-03-28 15:47:04 +0200

    Adding latest version of jupysql because of bug

--- create_python_package.sh
+++ create_python_package.sh
@@ -20,5 +20,4 @@ python3 -m pip install pandas numpy jproperties jupyter jupysql duckdb-engine
 # duckdb-engine installs main duckdb which we need to install to have our own duckdb version
 python3 -m pip uninstall duckdb
 
-
 EXTENSION_STATIC_BUILD=1 BUILD_SQLPGQ=1 BUILD_PYTHON=1 make GEN=ninja
