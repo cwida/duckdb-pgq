2bf558805e129fc0168e0fabf3a383416958fda7
dtenwolde <dljtw98@gmail.com>
2023-03-29 16:00:11 +0200

    Starting on physical and logical drop property graph operators

--- src/include/duckdb/common/enums/logical_operator_type.hpp
+++ src/include/duckdb/common/enums/logical_operator_type.hpp
@@ -81,6 +81,7 @@ enum class LogicalOperatorType : uint8_t {
 	LOGICAL_ATTACH = 136,
 	LOGICAL_DETACH = 137,
 	LOGICAL_CREATE_PROPERTY_GRAPH = 138,
+    LOGICAL_DROP_PROPERTY_GRAPH = 139,
 
 	// -----------------------------
 	// Explain
---
+++ src/include/duckdb/execution/operator/schema/physical_drop_property_graph.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "duckdb/execution/physical_operator.hpp"
+#include "duckdb/parser/parsed_data/drop_property_graph_info.hpp"
+
+namespace duckdb {
+
+
+
+}
