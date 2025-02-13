04d96105a41b81254d480b7b57a35d166a852017
dtenwolde <dljtw98@gmail.com>
2025-02-13 10:15:48 +0100

    Add pathfinding to benchmark

---
+++ benchmark/pathfinding/init/load.sql
@@ -0,0 +1,11 @@
+require duckpgq
+
+attach '/Users/dljtw/git/duckpgq/test-pathfinding.duckdb';
+use 'test-pathfinding';
+set experimental_path_finding_operator=true;
+create or replace table snb_pairs as (
+   select src, dst
+   from (select a.rowid as src from person a),
+        (select b.rowid as dst from person b)
+   using sample reservoir(16384 rows) repeatable (300)
+);
