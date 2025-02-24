d874a49114fa82566d774b88fe43a63441000dfe
Dtenwolde <dljtw@cwi.nl>
2025-02-24 14:14:54 +0100

    Fix uppercase

--- benchmark/pathfinding/get-small-data.sh
+++ benchmark/pathfinding/get-small-data.sh
@@ -17,7 +17,7 @@ fi
 
 for SF in 1 3 10 30 100 300; do
     curl --silent --fail --http1.1 https://pub-383410a98aef4cb686f0c7601eddd25f.r2.dev/duckpgq-experiments/snb-bi/snb-bi-sf${SF}.tar.zst | tar -xv --use-compress-program=unzstd
-    mv snb-bi-SF${SF}.v snb-bi.v
+    mv snb-bi-sf${SF}.v snb-bi.v
     mv snb-bi-sf${SF}.e snb-bi.e
     duckdb snb-bi-sf${SF}.duckdb -f ../queries/load.sql
     rm snb-bi.e snb-bi.v
