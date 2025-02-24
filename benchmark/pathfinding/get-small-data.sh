e1f409be3a717aad81159d895f0c0c6973968fa0
dtenwolde <dljtw98@gmail.com>
2025-02-24 14:00:14 +0100

    Also install duckdb if not installed already

--- benchmark/pathfinding/get-small-data.sh
+++ benchmark/pathfinding/get-small-data.sh
@@ -7,6 +7,14 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd 
 
 cd data
 
+# Check if DuckDB is installed
+if ! command -v duckdb &> /dev/null; then
+    echo "DuckDB not found. Installing..."
+    curl https://install.duckdb.org | sh
+else
+    echo "DuckDB is already installed."
+fi
+
 for SF in 1 3 10 30 100 300; do
     curl --silent --fail --http1.1 https://pub-383410a98aef4cb686f0c7601eddd25f.r2.dev/duckpgq-experiments/snb-bi/snb-bi-sf${SF}.tar.zst | tar -xv --use-compress-program=unzstd
     mv snb-bi-SF${SF}.v snb-bi.v
