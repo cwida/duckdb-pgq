fe0ee6d92b0b656f444215e2e2fea039b0ff460e
dtenwolde <dljtw98@gmail.com>
2025-03-11 17:21:54 +0100

    Add git commit to output, add todo about partitions

--- benchmark/pathfinding/run.sh
+++ benchmark/pathfinding/run.sh
@@ -12,4 +12,11 @@ rm benchmarks/*
 ./generate_benchmark_scripts.sh
 cd ../../../
 BUILD_BENCHMARK=1 make GEN=ninja
-build/release/benchmark/benchmark_runner --disable-timeout "benchmark/pathfinding/benchmarks/.*" > output.timing 2>&1
+
+COMMIT_HASH=$(git rev-parse --short HEAD)
+
+# Define output file with commit hash and timestamp
+OUTPUT_FILE="output_${COMMIT_HASH}.timing"
+
+# Run benchmark and save output
+build/release/benchmark/benchmark_runner --disable-timeout "benchmark/pathfinding/benchmarks/.*" > "$OUTPUT_FILE" 2>&1
