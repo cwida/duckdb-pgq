3f0dd2039ee312d075e944d8d6fe019f8bcca09f
dtenwolde <dljtw98@gmail.com>
2025-03-14 15:10:26 +0100

    [experiment] Recursive partitioning with parameter

--- benchmark/pathfinding/generate_benchmark_scripts.sh
+++ benchmark/pathfinding/generate_benchmark_scripts.sh
@@ -5,18 +5,18 @@ versions=("operator")
 sources=(16384)
 scale_factors=(1 3 10 30 100 300)
 threads=(1 2 4 8)
-
+partition_sizes=(65536 131072 262144 524288 1048576)
 # Loop over versions, sources, and scale factors
 for version in "${versions[@]}"; do
   for source in "${sources[@]}"; do
     for scale in "${scale_factors[@]}"; do
       for thread in "${threads[@]}"; do
+        for partition_size in "${partition_sizes[@]}"; do
+          # Define the file name
+          filename="benchmarks/${version}_sf${scale}_src${source}_t${thread}_p${partition_size}.benchmark"
 
-        # Define the file name
-        filename="benchmarks/${version}_sf${scale}_src${source}_t${thread}.benchmark"
-
-        # Write content to the file
-       cat <<EOL > "$filename"
+          # Write content to the file
+          cat <<EOL > "$filename"
 # name: benchmark/pathfinding/${version}.benchmark
 # description: Run ${version} Query For Pathfinding Benchmark
 # group: [pathfinding]
@@ -26,9 +26,11 @@ QUERY_NAME=${version}
 NUMBER_OF_SOURCES=${source}
 SCALE_FACTOR=${scale}
 THREADS=${thread}
+PARTITION_SIZE=${partition_size}
 EOL
 
         echo "Generated: $filename"
+        done
       done
     done
   done
